b68a7cbec156d17f11121dd29fa848549cb4f1ec
Mark Davis <mark@colovo.com>
2017-01-04 17:07:15 -0400

    Update server

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ let isInitialCompilation = true;
 
 const compiler = webpack(config);
 
+const port = process.env.PORT || config.port
+
 new WebpackDevServer(compiler, config.devServer)
-.listen(config.port, 'localhost', (err) => {
+.listen(port, 'localhost', (err) => {
   if (err) {
     console.log(err);
   }
@@ -27,8 +29,8 @@ compiler.plugin('done', () => {
     // Ensures that we log after webpack printed its stats (is there a better way?)
     setTimeout(() => {
       console.log('\n✓ The bundle is now ready for serving!\n');
-      console.log('  Open in iframe mode:\t\x1b[33m%s\x1b[0m',  'http://localhost:' + config.port + '/webpack-dev-server/');
-      console.log('  Open in inline mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + config.port + '/\n');
+      console.log('  Open in iframe mode:\t\x1b[33m%s\x1b[0m',  'http://localhost:' + port + '/webpack-dev-server/');
+      console.log('  Open in inline mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + port + '/\n');
       console.log('  \x1b[33mHMR is active\x1b[0m. The bundle will automatically rebuild and live-update on changes.')
     }, 350);
   }
